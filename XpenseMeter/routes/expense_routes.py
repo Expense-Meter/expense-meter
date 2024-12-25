a202daf3583339efa222207a321cf68cbbe539c6
Aman Agrawal <582002aman@gmail.com>
2024-12-25 18:32:26 +0530

    TSPACE

--- XpenseMeter/routes/expense_routes.py
+++ XpenseMeter/routes/expense_routes.py
@@ -3,6 +3,7 @@ from XpenseMeter.services.finance_services import create_new_expense, get_expens
 
 
 router = APIRouter()
+
 router.add_api_route('/', create_new_expense, methods=["GET"])
 router.add_api_route('/expenses', get_expense, methods=["GET"])
 router.add_api_route('/expenses', add_expense, methods=["POST"])
